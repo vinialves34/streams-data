48c90bce0e89922ea0caf035574f012d8caba9ae
Vinicius Alves <vinialves.dev@gmail.com>
2023-02-10 18:13:12 -0300

    chore: add scripts for request

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import { createServer } from "node:http";
 import { createReadStream } from "node:fs";
-import { Readable } from "node:stream";
+import { Readable, Transform } from "node:stream";
 import { WritableStream } from "node:stream/web";
+import { setTimeout } from "node:timers/promises";
+
+import csvtojson from "csvtojson";
 
 const PORT = 3000
 createServer(async (request, response) => {
@@ -16,10 +19,31 @@ createServer(async (request, response) => {
     return;
   }
 
+  request.once("close", _=> console.log(`connection was closed`, items));
+
   let items = 0;
   Readable.toWeb(createReadStream("./mocks/users.csv"))
+  .pipeThrough(Transform.toWeb(csvtojson()))
+  .pipeThrough(new TransformStream({
+    transform(chunk, controller) {
+      const {
+        first_name,
+        last_name,
+        email,
+        avatar
+      } = JSON.parse(Buffer.from(chunk));
+      const mappedData = {
+        fullname: `${first_name} ${last_name}`,
+        email,
+        avatar
+      };
+
+      controller.enqueue(JSON.stringify(mappedData).concat("\n"));
+    }
+  }))
   .pipeTo(new WritableStream({
-    write(chunk) {
+    async write(chunk) {
+      await setTimeout(1000)
       items++
       response.write(chunk);
     },
